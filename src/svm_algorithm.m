03c13c9a3372d3b7de3e9b9195293f92e8fb44f2
mambla <roee.sheffer@gmail.com>
2015-02-18 00:17:47 +0200

    refactoring

--- src/svm_algorithm.m
+++ src/svm_algorithm.m
@@ -1,16 +1,22 @@
-function [ algorithm ] = svm_algorithm(svm_options)
+function [ algorithm ] = svm_algorithm(svm_options, dim_radius)
 
 algorithm.train = @train;
 algorithm.classify = @classify;
 algorithm.description = sprintf('svm, options = %s', svm_options);
 algorithm.params.svm_options = svm_options;
+algorithm.params.dim_radius = dim_radius;
 
 end
 
-% successful params: [ accuracy_vec, best_algorithm ] = evaluate_algorithms(svm_algorithm('-t 2 -g 0.007 -c 10 -q'), train, extracted_train(:,91:111,:), 0, missing);
+% successful params: svm_algorithm('-t 2 -g 0.007 -c 100 -q',20)
 function [ model ] = train(params, train, extracted_train, snp_positions, missing)
 
 model.svm_models = cell(length(missing),1);
+model.dim_radius = params.dim_radius;
+
+middle = (size(extracted_train,2) + 1 )/2;
+
+extracted_train = extracted_train(:,middle-params.dim_radius:middle+params.dim_radius,:);
 
 for i = 1:length(missing)
     model.svm_models{i} = train_single_svm_model(...
@@ -35,6 +41,10 @@ end
 function [ ytest ] = classify(model, test, extracted_test, snp_positions, missing)
 
 ytest = zeros(length(missing), size(test,2));
+
+
+middle = (size(extracted_test,2) + 1 )/2;
+extracted_test = extracted_test(:,middle-model.dim_radius:middle+model.dim_radius,:);
 
 for i = 1:length(missing)
     extracted_test_snp = squeeze(extracted_test(i,:,:));
