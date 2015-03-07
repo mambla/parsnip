8dbe08fb53d24e478e76ad12908a82821332a079
shahar-dav <davidson.shahar@gmail.com>
2015-03-07 21:49:42 +0200

    add decision tree algo

---
+++ src/exampleTree.m
@@ -0,0 +1,12 @@
+radius = 10;
+tree = tree_algorithm(radius,0);
+
+K=3;
+%save time, run all the process only on snp 1 to <snp_to_test>
+snp_to_test = 300;
+%the adaboost doesn't use the all train or all test. only the extracted
+%data. I passed dummy matrixes to save time.
+[error, errorV] = cross_validate(tree,train(1,:),extracted_train,[],missing(1:snp_to_test),K);
+
+%Error is mean error for all snp
+%errorV is the mean error for each separate snp tested
