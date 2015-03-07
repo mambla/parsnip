db79b3d0c9728b1c89adff3a0bc8f12f79005bf7
mambla <roee.sheffer@gmail.com>
2015-03-08 01:09:33 +0200

    ghgg

---
+++ final/src/go_histogram.m
@@ -0,0 +1,7 @@
+function [ ytest ] = go_histogram()
+
+% polynomial svm of degree=2 and c=1 is used upon descriptors containing
+% histograms of all sequences of length 4 (sliding window interval is 1)
+ytest = go_generic(histogram_algorithm('-t 1 -d 2 -c 1 -q',4,1));
+
+end
