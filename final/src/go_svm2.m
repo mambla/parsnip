e29356ad2e61d419399a71d882cfe5457de823bb
shahar-dav <davidson.shahar@gmail.com>
2015-03-08 02:08:07 +0200

    added to go.m more algorithm instances

--- final/src/go_svm2.m
+++ final/src/go_svm2.m
@@ -1,9 +1,9 @@
 function [ ytest ] = go_svm2()
 
-% radial svm with g = 0.007 and c = 100 upon the 9 SNPs before and after
+% radial svm with g = 0.01 and c = 100 upon the 30 SNPs before and after
 % the one we are looking for
 % take the most 10 correlated permutated snps as features
 
-ytest = go_generic(svm_algorithm2('-t 2 -g 0.007 -c 100 -q', 9, 10));
+ytest = go_generic(svm_algorithm2('-t 2 -g 0.01 -c 100 -q', 30, 10));
 
 end
